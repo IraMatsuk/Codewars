c21cf63bd9e1f4ffa928b4921a870ee9b3c8e9a6
Ira Matsuk <g3726866@gmail.com>
2020-02-12 22:33:57 +0300

    feat: implementation of the project that calculates the amount of water for a cyclist

---
+++ Mathematics/KeepHydrated/src/KeepHydrated.java
@@ -0,0 +1,5 @@
+public class KeepHydrated {
+	public int liters(double time) {
+		return (int) Math.floor(time * 0.5);
+	}
+}
---
+++ Mathematics/KeepHydrated/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+	public static void main(String[] args) {
+		KeepHydrated keepHydrated = new KeepHydrated();
+		System.out.println(keepHydrated.liters(0.89));
+		System.out.println(keepHydrated.liters(12.87));
+		System.out.println(keepHydrated.liters(166.2));
+	}
+}
