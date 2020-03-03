007ce12c2af9ed8f28572952b57160011b7b0475
Ira Matsuk <g3726866@gmail.com>
2020-03-03 19:39:26 +0300

    feat: create method that calculate volume of a cuboid

---
+++ Algirithms/8.VolumeOfACuboid/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {	  	  
+	      System.out.println(Kata.getVolumeOfCuboid(1, 2, 2));
+	      System.out.println(Kata.getVolumeOfCuboid(6.3, 2, 5));	   
+	}
+}
